290b47e06790b1c7a3bad662003fdbea74c2fd06
João Rios <101368524+joaor1os@users.noreply.github.com>
2022-03-24 17:17:51 +0000

    new version

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from flask import Flask
-app = Flask('app')
+app = Flask('__name__')
 
 @app.route('/')
 def minha_primira_rota():
   return '<h1>Minha primeira rota!</h1>'
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0')
